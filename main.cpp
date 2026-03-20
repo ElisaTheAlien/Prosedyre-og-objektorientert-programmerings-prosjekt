ba818ca876d76663f60c1626935625847afc1d03
ElisaTheAlien <Elisa.re@online.no>
2026-03-20 17:53:36 +0100

    Changed main function in maincpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,9 @@
-//
-// This is example code from Chapter 2.2 "The classic first program" of
-// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
-//
-// This program outputs the message "Hello, World!" to the monitor
+#include <iostream>
 
-#include "std_lib_facilities.h"
-
-//------------------------------------------------------------------------------'
-
-// C++ programs start by executing the function main
 int main() {
-    // Show a nice message in the Terminal window
-    cout << "Hello, World!" << endl;
 
-    // This lets the operating system (Windows, Mac, Linux, etc.) know that the program
-    // did not encounter any errors
+    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
 
-//------------------------------------------------------------------------------
