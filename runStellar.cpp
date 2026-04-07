cb5bfcce0d64b99a88217dedadcc07aa8889daae
AMERismoen <amerismoen@gmail.com>
2026-04-07 17:36:52 +0200

    Begynt på runStellar::run

--- runStellar.cpp
+++ runStellar.cpp
@@ -1,8 +1,9 @@
 #include "runStellar.h"
 
 void run(TDT4102::AnimationWindow& window){
-    
+    startScreen start("Startscreen/welcome.txt", "Startscreen/earth.png");
+    start.animate(window);
     while(!quit){
-
+        start.draw(window);
     }
 }
