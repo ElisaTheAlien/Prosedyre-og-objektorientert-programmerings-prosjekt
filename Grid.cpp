3d0880f2c39c0fec7e1a5e9a08df71c3e043e6a0
ElisaTheAlien <Elisa.re@online.no>
2026-04-07 21:13:59 +0200

    grid got some grid

--- Grid.cpp
+++ Grid.cpp
@@ -1,5 +1,20 @@
 #include "Grid.h"
 
 void Grid(TDT4102::AnimationWindow& window){
-    
+
+    double winWidth = window.width();
+    double winHeight = window.height();
+
+    const TDT4102::Point buttonPosition1 {0, 0};
+    const TDT4102::Point buttonPosition2 {winWidth/2, 0};
+    const unsigned int buttonWidth = winWidth/2;;
+    const unsigned int buttonHeight = winHeight;
+    const std::string buttonLabel = "";
+
+    TDT4102::Button button1 {buttonPosition1, buttonWidth, buttonHeight, buttonLabel};
+
+
+    TDT4102::Button button2 {buttonPosition2, buttonWidth, buttonHeight, buttonLabel};
+
+    window.wait_for_close();
 }
--- Grid.h
+++ Grid.h
@@ -1,2 +1,4 @@
 #include "AnimationWindow.h"
+#include "widgets/Button.h"
+
 void Grid(TDT4102::AnimationWindow& window);
