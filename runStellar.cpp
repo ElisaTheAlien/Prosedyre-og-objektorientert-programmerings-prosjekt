dc61e9350554b45a9f4ca55be2153704857f1996
ElisaTheAlien <Elisa.re@online.no>
2026-04-07 17:26:41 +0200

    Endirnger i runStellar h og cpp fil

---
+++ runStellar.cpp
@@ -0,0 +1,8 @@
+#include "runStellar.h"
+
+void run(TDT4102::AnimationWindow& window){
+    
+    while(!quit){
+
+    }
+}
---
+++ runStellar.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "AnimationWindow.h"
+#include "widgets/Button.h"
+#include <iostream>
+#include <string>
+class runStellar{
+
+    private:
+        bool quit = false;
+        bool button1Pressed = false;
+        bool button2Pressed = false;
+
+    public:
+        void callback();
+        void run(TDT4102::AnimationWindow& window);
+};
