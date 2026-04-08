9f9b0ff2e3b1824d3b9fceb9f52f8fab4d7f66e0
ElisaTheAlien <Elisa.re@online.no>
2026-04-08 19:12:22 +0200

    Grid ain't gridding

--- Grid.h
+++ Grid.h
@@ -1,4 +1,12 @@
 #include "AnimationWindow.h"
 #include "widgets/Button.h"
 
+class Grid : public TDT4102::AnimationWindow {
+    public:
+        Grid(int width, int height, const std::string& title) : TDT4102::AnimationWindow(width, height, title){
+
+        }
+
+};
+
 void Grid(TDT4102::AnimationWindow& window);
