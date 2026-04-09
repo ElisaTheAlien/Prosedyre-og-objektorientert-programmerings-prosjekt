161b6fab82b7d016ac07fa7e12cdb24d3bcdb679
ElisaTheAlien <Elisa.re@online.no>
2026-04-09 11:49:52 +0200

    Default consturcotrs

--- GridConstillation.h
+++ GridConstillation.h
@@ -3,7 +3,6 @@
 
 class GridConstillation : public TDT4102::AnimationWindow {
     public:
-        GridConstillation() = default;
         void GridMaking(TDT4102::AnimationWindow& window);
         void CallbackButton1();
         void CallbackButton2();
