79fb2fa3a5f3e9212fe0e09f5492ca470aaacc57
AMERismoen <amerismoen@gmail.com>
2026-04-10 18:29:49 +0200

    Test av runStellar

--- main.cpp
+++ main.cpp
@@ -4,14 +4,10 @@
 #include "startScreen.h"
 
 int main() {
-    startScreen screen("Startscreen/welcome.txt", "Startscreen/earth.png");
     TDT4102::AnimationWindow win (0,0,300,200); 
     TDT4102::AnimationWindow* ptr = &win;
-    while (!win.should_close()){
-          screen.animation(*ptr);
-    }
-    
-
+    runStellar Stellar;
+    Stellar.run(*ptr);
     return 0;
 }
 
