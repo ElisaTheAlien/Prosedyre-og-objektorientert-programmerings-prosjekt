ea0bb17f3b3cd9796f3900ccb0febc509b5e7609
ElisaTheAlien <Elisa.re@online.no>
2026-04-10 10:28:48 +0200

    Fjernet buttons fra grid klassene, må lages manuelt.

--- GridConstillation.h
+++ GridConstillation.h
@@ -9,10 +9,6 @@ class GridConstillation : public TDT4102::AnimationWindow {
         bool show1 = false;
         bool show2 = false;
 
-    protected:
-
-        TDT4102::Button button1;
-        TDT4102::Button button2;
 };
 
 
